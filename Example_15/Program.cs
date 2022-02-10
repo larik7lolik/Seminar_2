4310c392a1e02dc7633c46453386feef59fcb07c
LarisaZaripova <larik7lolik@gmail.com>
2022-02-10 19:54:03 +0300

    added Example 15

---
+++ Example_15/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 15: Дано число. Проверить кратно ли оно 7 и 23
+Console.WriteLine("Ввести число: ");
+int num = int.Parse(Console.ReadLine());
+if(num % 7 ==0 && num % 23 == 0)
+{
+    Console.WriteLine("Число " + num + " кратно 7 и 23");
+    return;
+}
+else
+{
+   Console.WriteLine("Число " + num + " Не кратно 7 и 23");
+}
