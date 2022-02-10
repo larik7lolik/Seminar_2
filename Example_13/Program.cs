64f6caf472e0bb4681b52895f59e67df4b29ceab
LarisaZaripova <larik7lolik@gmail.com>
2022-02-10 12:34:36 +0300

    added Example 13

---
+++ Example_13/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 13: Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int numA = 4;
+if(num % numA == 0)
+{
+    Console.WriteLine("Число кратно");
+}
+else 
+{
+   Console.WriteLine("Число равно " + num % numA);
+}
+
