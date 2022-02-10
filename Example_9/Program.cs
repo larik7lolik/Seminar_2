1e7b354e80b5579a71935e1fd1e6f98a23e7c901
LarisaZaripova <larik7lolik@gmail.com>
2022-02-10 11:59:19 +0300

    added new Example 9

--- Example_9/Program.cs
+++ Example_9/Program.cs
@@ -1,5 +1,14 @@
 ﻿// Задача 9: Показать последнюю цифру трёхзначного числа
-Console.WriteLine("Введите трёхзначное число: ");
+/*Console.WriteLine("Введите трёхзначное число: ");
 int num = int.Parse(Console.ReadLine());
 int result = num % 10;
-Console.WriteLine(result);
+Console.WriteLine(result);*/
+
+int EndNumber(int number)
+{
+    int result = number % 10;
+    return result;
+}
+Console.WriteLine( "Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine("Последняя цифра: " + EndNumber(num));
